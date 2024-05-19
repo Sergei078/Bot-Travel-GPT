fe706fa653dc43af95c0da26435661a05bd1971c
Артём Сергеевич <artemsavelev356@gmail.com>
2024-05-19 18:44:17 +0300

    обновление config файла

--- config.py
+++ config.py
@@ -4,3 +4,14 @@ from os import getenv
 load_dotenv()
 TOKEN = getenv('TOKEN')
 GEO_TOKEN = getenv('GEO_TOKEN')
+FOLDER_ID = ''
+IAM_TOKEN = ''
+ADMINS_IDS = ''
+MAX_GPT_TOKENS = 120
+TOKENIZE_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
+GPT_MODEL = 'yandexgpt-lite'
+GPT_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+LOGS = 'creds/logs.log'
+SYSTEM_PROMPT = [{'role': 'system', 'text':"Ответы должны быть актуальными, информативными и привлекательно "
+                                           "оформленными. Включи в ответ следующие элементы: краткое описание места, "
+                                           "текущую погоду и советы по планированию путешествий. "}]
