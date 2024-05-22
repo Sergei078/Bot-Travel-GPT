0d6c0d3f5ef652685f147c29bf09f6f524b7bf53
Артём Сергеевич <artemsavelev356@gmail.com>
2024-05-22 15:18:10 +0300

    обработка статус кодов

--- geoloc.py
+++ geoloc.py
@@ -1,6 +1,10 @@
 import requests
 from config import GEO_TOKEN
-from log import logger
+import logging
+
+logging.basicConfig(filename='logConfig.log', encoding='utf-8', level=logging.WARNING,
+                    format="%(asctime)s FILE: %(filename)s IN: %(funcName)s MESSAGE: %(message)s")
+logger = logging.getLogger(__name__)
 
 
 def i_know_where_you_live(lat, long):
@@ -13,10 +17,13 @@ def i_know_where_you_live(lat, long):
         # address['address']['county_code'] - код страны по стандарту ISO 3166-1 alpha-2, поможет определить язык?
         city = address['address'].get('city')
         country = address['address'].get('country')
-        return True, city, country
+        if address.status_code == 200:
+            return True, city, country
+        else:
+            logger.error(address.status_code)
+            return False, f'ошибка в получении геоданных: {address.status_code}'
 
     except Exception as e:
         err_msg = f'ошибка: {e}'
         logger.error(err_msg)
-
-        return False, 'ошибка в получении геоданных'
+        return False, 'что то пошло не так'
