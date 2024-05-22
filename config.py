9d84da6d80a225331ea7d0bdcc5e2c565a940b02
Matvejka <152027855+Motyavpotoke@users.noreply.github.com>
2024-05-22 22:19:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,23 @@ from os import getenv
 load_dotenv()
 TOKEN = getenv('TOKEN')
 GEO_TOKEN = getenv('GEO_TOKEN')
-FOLDER_ID = ''
-IAM_TOKEN = ''
+API_WEATHER = getenv('API_WEATHER')
+FOLDER_ID = getenv('FOLDER_ID')
+IAM_TOKEN = getenv('IAM_TOKEN')
 ADMINS_IDS = ''
+MAX_GPT_TOKENS = 5000
+TOKENIZE_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
 GPT_MODEL = 'yandexgpt-lite'
 GPT_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-LOGS = 'log/logs.log'
-SYSTEM_PROMPTS = {
-    'features_city': "Проанализируй и предоставь подробное описание особенностей и интересных мест следующего города:",
+LOGS = 'creds/logs.log'
+SYSTEM_PROMPT = {
+    'features_city': "Проанализируй и предоставь подробное описание достопримечательностей и интересных мест "
+                     "следующего города:",
+    'interesting_facts': "Перечисли интересные факты о следующем городе:",
     'clothes': "На основе текущих метеорологических данных: температура, скорость ветра и уровень влажности, "
-               "предложи подходящие варианты одежды для нахождения на улице. Дополнительно учти следующее описание "
-               "ситуации:",
+               "предложи подходящие варианты одежды для нахождения на улице.",
     'beautiful_presentation': "Сформулируй информативный и визуально привлекательный текст о текущей погоде в "
-                              "указанном городе,"
-                              "включая температуру, ветер и влажность, используя смайлики для описания каждого "
-                              "погодного условия."
+                              "указанном городе, включая температуру, ветер и влажность, используя смайлики для "
+                              "описания каждого погодного условия."
 }
+
