fd6d1d0ef34289df1b335d8bcbc1dd76a1dec04a
Артём Сергеевич <artemsavelev356@gmail.com>
2024-05-23 16:07:54 +0300

    больше ключей

--- geoloc.py
+++ geoloc.py
@@ -6,24 +6,30 @@ logging.basicConfig(filename='logConfig.log', encoding='utf-8', level=logging.WA
                     format="%(asctime)s FILE: %(filename)s IN: %(funcName)s MESSAGE: %(message)s")
 logger = logging.getLogger(__name__)
 
+keys_list = ['city', 'town', 'village', 'state', 'district']
+
 
 def i_know_where_you_live(lat, long):
     try:
         headers = {'Accept-Language': 'ru'}
-        address = requests.get(
+        req = requests.get(
             f'https://eu1.locationiq.com/v1/reverse.php?key={GEO_TOKEN}&lat={lat}&lon={long}&format=json',
             headers=headers).json()
 
-        # address['address']['county_code'] - код страны по стандарту ISO 3166-1 alpha-2, поможет определить язык?
-        city = address['address'].get('city')
-        country = address['address'].get('country')
-        if address.status_code == 200:
+        # req['req']['county_code'] - код страны по стандарту ISO 3166-1 alpha-2, поможет определить язык?
+
+        city = 'не найдено'
+        for key in keys_list:
+            if key in req['address']:
+                city = req['address'].get(key)
+
+        country = req['address'].get('country')
+        if req.status_code == 200:
             return True, city, country
         else:
-            logger.error(address.status_code)
-            return False, f'ошибка в получении геоданных: {address.status_code}'
+            logger.error(req.status_code)
+            return False, f'ошибка в получении геоданных'
 
     except Exception as e:
-        err_msg = f'ошибка: {e}'
-        logger.error(err_msg)
+        logger.error(f'ошибка: {e}')
         return False, 'что то пошло не так'
